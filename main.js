86bfe79b98b90f064bfd14e3a5cb743475d24890
urielbang <uri32655@gmail.com>
2023-09-13 13:57:09 +0300

    js changes

--- main.js
+++ main.js
@@ -15,9 +15,43 @@
 // });
 
 //3
-const lastNames = ["abramov", "levi", "cohen", "meshulam", "davidov", "babyev"];
-let newLastNames = [];
-lastNames.forEach((item) => {
-  newLastNames.push(item.toUpperCase());
-});
-console.log(newLastNames);
+// const lastNames = ["abramov", "levi", "cohen", "meshulam", "davidov", "babyev"];
+// let newLastNames = [];
+// lastNames.forEach((item) => {
+//   newLastNames.push(item.toUpperCase());
+// });
+// console.log(newLastNames);
+
+//4
+
+// const lastNames = ["abramov", "levi", "cohen", "meshulam", "davidov"];
+// for (const nameItem of lastNames) {
+//   console.log(nameItem);
+// }
+
+//5
+
+// const ages = [50, 20, 30, 60, 40, 80, 60, 30, 20, 10.56];
+// for (const itemAge of ages) {
+//   if (itemAge > 20) {
+//     console.log(itemAge);
+//   }
+// }
+
+//6
+// const lastNames = ["abramov", "levi", "cohen", "meshulam", "davidov", "babyev"];
+// let newLAstNAmes = [];
+
+// for (const itemLAstName of lastNames) {
+//   newLAstNAmes.push(itemLAstName.toUpperCase());
+// }
+
+//7
+
+// document.body.innerHTML += `<p>uriel</p>
+// <p>daniel</p>
+// <p>shmhel</p>
+// <p>lior</p>
+// <p>shlomo</p>
+// <button id="btnColor">click here! to color</button>
+// <button id="btnColor">click here! to hello</button>`;
