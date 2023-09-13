fb1a733b0a544c69b0f89acbde9c7c0c00516a1e
urielbang <uri32655@gmail.com>
2023-09-13 21:10:58 +0300

    task10

--- main.js
+++ main.js
@@ -47,21 +47,121 @@
 // }
 
 //7
-document.body.innerHTML += `    <ul>
-<li>israel</li>
-<li>austrita</li>
-<li>america</li>
-<li>russia</li>
-<li>kanda</li>
-<li>georgia</li>
-</ul>
-<button id="btn">click to upper</button>
-`;
-const ulCollection = document.getElementsByTagName("li");
+// document.body.innerHTML += `    <ul>
+// <li>israel</li>
+// <li>austrita</li>
+// <li>america</li>
+// <li>russia</li>
+// <li>kanda</li>
+// <li>georgia</li>
+// </ul>
+// <button id="btn">click to upper</button>
+// `;
+// const ulCollection = document.getElementsByTagName("li");
 
-btn.addEventListener("click", () => {
-  for (const itemLi of ulCollection) {
-    let strTmp = itemLi.innerText.toUpperCase();
-    itemLi.innerText = strTmp;
-  }
-});
+// btn.addEventListener("click", () => {
+//   for (const itemLi of ulCollection) {
+//     let strTmp = itemLi.innerText.toUpperCase();
+//     itemLi.innerText = strTmp;
+//   }
+// });
+
+//8
+
+// const home = {
+//   addresse: "aba ahimeir",
+//   city: "ramle",
+//   country: "israel",
+//   numRooms: 4,
+//   isresidents: false,
+// };
+// console.log(home);
+
+// for (const key in home) {
+//   console.log(key);
+// }
+// for (const key in home) {
+//   document.body.innerHTML += home[key];
+//   document.body.innerHTML += `<br>`;
+// }
+
+//9
+// const dogsObjects = [
+//   { name: "laki", age: 2, type: "pug" },
+//   { name: "mori", age: 3, type: "lori" },
+//   { name: "doli", age: 4, type: "grman" },
+// ];
+//1
+// function printLogObjects() {
+//   for (const item of dogsObjects) {
+//     console.log(item);
+//   }
+// }
+//2
+// function printTheSeconDog() {
+//   for (const keyDog in dogsObjects[1]) {
+//     console.log(dogsObjects[1][keyDog]);
+//   }
+// }
+//3
+// for (const itemDogs of dogsObjects) {
+//   document.body.innerHTML += `<div id="id${dogsObjects.indexOf(
+//     itemDogs
+//   )}"</div>`;
+//   let divId = document.getElementById(`id${dogsObjects.indexOf(itemDogs)}`);
+//   for (const key in itemDogs) {
+//     divId.innerHTML += `<h3>${key}:${itemDogs[key]}</h3>`;
+//   }
+// }
+
+//10
+// const studentsArray = [
+//   {
+//     name: "uriel",
+//     date: "15.03.98",
+//     email: "uri@gmail.com",
+//     isPasst: false,
+//   },
+//   {
+//     name: "beni",
+//     date: "16.03.99",
+//     email: "veni@gmail.com",
+//     isPasst: true,
+//   },
+//   {
+//     name: "shkoni",
+//     date: "14.03.2016",
+//     email: "shkoni@gmail.com",
+//     isPasst: false,
+//   },
+//   {
+//     name: "michael",
+//     date: "20.05.1975",
+//     email: "michael@gmail.com",
+//     isPasst: true,
+//   },
+// ];
+
+// function printEvereStudent() {
+//   for (const itemStudent of studentsArray) {
+//     console.log(itemStudent);
+//   }
+// }
+
+// function prinTheardStudents() {
+//   for (const key in studentsArray[2]) {
+//     console.log(studentsArray[2][key]);
+//   }
+// }
+// function printEveryStudent() {
+//   for (const itemStudetn of studentsArray) {
+//     document.body.innerHTML += `<div id="div_${studentsArray.indexOf(
+//       itemStudetn
+//     )}"></div>`;
+//     for (const key in itemStudetn) {
+//       document.getElementById(
+//         `div_${studentsArray.indexOf(itemStudetn)}`
+//       ).innerHTML += ` ${key}: ${itemStudetn[key]}`;
+//     }
+//   }
+// }
